0c3fc9ef0452c61d79b85b324f8bc05ccabb64b1
murwell <dfklein@sympile.com>
2022-01-23 13:34:08 -1000

    Update HolocaustTimeline.php

--- HolocaustTimeline.php
+++ HolocaustTimeline.php
@@ -17,50 +17,17 @@
 
 declare(strict_types=1);
 
-use Fisharebest\Localization\Translation;
+namespace Fisharebest\Webtrees\Module;
+
 use Fisharebest\Webtrees\I18N;
-use Fisharebest\Webtrees\Module\AbstractModule;
-use Fisharebest\Webtrees\Module\ModuleCustomInterface;
-use Fisharebest\Webtrees\Module\ModuleCustomTrait;
-use Fisharebest\Webtrees\Module\ModuleHistoricEventsTrait;
-use Fisharebest\Webtrees\Module\ModuleHistoricEventsInterface;
 use Illuminate\Support\Collection;
 
-/** 
- * Historical Holocaust Timeline
-  */
-return new class extends AbstractModule implements ModuleCustomInterface, ModuleHistoricEventsInterface {
-    use ModuleCustomTrait;
+/**
+ * Class HolocaustTimeline
+ */
+class HolocaustTimeline extends AbstractModule implements ModuleHistoricEventsInterface
+{
     use ModuleHistoricEventsTrait;
-
-    public const CUSTOM_TITLE = 'Holocaust Timeline';
-    
-    public const CUSTOM_WEBSITE = 'https://github.com/murwell/Holocuast-Timeline/';
-    
-    public const CUSTOM_VERSION = '1.0.0';
-
-    /**
-     * Constructor.  The constructor is called on *all* modules, even ones that are disabled.
-     * This is a good place to load business logic ("services").  Type-hint the parameters and
-     * they will be injected automatically.
-     */
-    public function __construct()
-    {
-        // NOTE:  If your module is dependent on any of the business logic ("services"),
-        // then you would type-hint them in the constructor and let webtrees inject them
-        // for you.  However, we can't use dependency injection on anonymous classes like
-        // this one. For an example of this, see the example-server-configuration module.
-    }
-
-    /**
-     * Bootstrap.  This function is called on *enabled* modules.
-     * It is a good place to register routes and views.
-     *
-     * @return void
-     */
-    public function boot(): void
-    {
-    }
 
     /**
      * How should this module be identified in the control panel, etc.?
@@ -69,17 +36,7 @@ return new class extends AbstractModule implements ModuleCustomInterface, Module
      */
     public function title(): string
     {
-        return self::CUSTOM_TITLE;
-    }
-
-        /**
-     * The version of this module.
-     *
-     * @return string
-     */
-    public function customModuleVersion(): string
-    {
-        return self::CUSTOM_VERSION;
+        return 'Holocaust Timeline';
     }
 
     /**
@@ -93,38 +50,17 @@ return new class extends AbstractModule implements ModuleCustomInterface, Module
     }
 
     /**
-     * Where does this module store its resources
-     *
-     * @return string
-     */
-    public function resourcesFolder(): string
-    {
-        return __DIR__ . '/resources/';
-    }
-    
-  
-    /**
-     * Structure of events provided by this module:
-     * 
-     * Each line is a GEDCOM style record to describe an event (EVEN), including newline chars (\n);
-     *      1 EVEN <name> (<party>)
-     *      2 TYPE <Chancellor|President> of Germany
-     *      2 DATE <date period>
-     *      2 NOTE [wikipedia de](<link>)
-     *
-     * markdown is used for NOTE;
-     * markdown should be enabled for your tree (see Control panel / Manage family trees / Preferences and then scroll down to "Text" and mark the option "markdown");
-     * is markdown is disabled the links are still working (blank at the end is necessary), but the formatting isn't so nice;
-     */
-
-
-    /**
      * All events provided by this module.
      *
      * @return Collection<string>
      */
+     
     public function historicEventsAll(): Collection
     {
+    switch (I18N::languageTag()) {
+            case 'en-AU':
+            case 'en-GB':
+            case 'en-US':
         return new Collection([
             "1 EVEN Germany Invades Belgium\n2 TYPE Occupation\n2 DATE @#DJULIAN@ 10 MAY 1940",
             "1 EVEN Germany Occupies Czechoslovakia\n2 TYPE Occupation\n2 DATE @#DJULIAN@ 15 MARCH 1939",
@@ -134,7 +70,12 @@ return new class extends AbstractModule implements ModuleCustomInterface, Module
             "1 EVEN Germany Occupies Hungary\n2 TYPE Occupation\n2 DATE @#DJULIAN@ 19 MARCH 1944",
             "1 EVEN Hitler Comes To Power\n2 TYPE Background\n2 DATE @#DJULIAN@ 01 JAN 1933",
             "1 EVEN Kristallnact\n2 TYPE Pogrom\n2 DATE FROM @#DJULIAN@ 09 NOV 1938 TO @#DJULIAN@ 10 NOV 1938\PLAC Germany\n2 NOTE Over 7,000 Jewish businesses were damaged or destroyed, and 30,000 Jewish men were arrested and incarcerated in concentration camps.",
-            
+           
         ]);
+        
+        default:
+                return new Collection();
+        }
+        
     }
 }
