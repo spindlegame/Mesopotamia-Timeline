dc637b92fe30b26f66c2aa1278b151e22ac35c5b
murwell <dfklein@sympile.com>
2022-01-24 18:00:58 -1000

    Update module.php

--- module.php
+++ module.php
@@ -123,14 +123,19 @@ return new class extends AbstractModule implements ModuleCustomInterface, Module
             "1 EVEN Japan Bombs Pearl Harbor\n2 TYPE Attack\n2 DATE 07 DEC 1941",
             "1 EVEN D-Day, Allies Invade Normandy\n2 TYPE Attack\n2 DATE 06 JUN 1944",
             "1 EVEN Germany Occupies Hungary\n2 TYPE Invasion\n2 DATE 19 MARCH 1944",
-            "1 EVEN Hitler Comes To Power\n2 TYPE Background\n2 DATE 01 JAN 1933",
+            "1 EVEN Hitler Becomes German Chancellor\n2 TYPE Background\n2 DATE 30 JAN 1933",
             "1 EVEN Great Britain and France Declare War Against Germany\n2 TYPE Declaration Of War\n2 DATE 03 SEP 1939",
             "1 EVEN Germany Invades Poland\n2 TYPE Invasion\n2 DATE 01 SEP 1939",
             "1 EVEN Auschwitz Established\n2 TYPE Concentration Camp\n2 DATE 20 MAY 1940",
             "1 EVEN Krakow Ghetto Established\n2 TYPE Ghetto\n2 DATE 03 MARCH 1941",
             "1 EVEN Germany begins the deportation of Dutch Jews from camps in the Netherlands\n2 TYPE Deportation\n2 DATE 15 JULY 1942",
             "1 EVEN Krakow Ghetto Liquidated\n2 TYPE Ghetto\n2 DATE 13 MARCH 1943",
-            "1 EVEN 400,000 Hungarian Jews Are Deported To Auschwitz\n2 TYPE Deportation\n2 DATE FROM 15 MAY 1944 TO 09 JULY 1944",
+            "1 EVEN 435,000 Hungarian Jews Are Deported To Auschwitz\n2 TYPE Deportation\n2 DATE FROM 15 MAY 1944 TO 09 JULY 1944",
+            "1 EVEN German Parliament Passes Nuremberg Race Laws\n2 TYPE Background\n2 DATE 15 SEP 1935",
+            "1 EVEN Kristallnacht\n2 TYPE Pogrom\n2 DATE FROM 09 NOV 1938 TO 10 NOV 1938\n1 NOTE The name Kristallnacht (literally "Crystal Night") comes from the shards of broken glass that littered the streets after the windows of Jewish-owned stores, buildings and synagogues were smashed.",
+
+            
+
 
 
 
